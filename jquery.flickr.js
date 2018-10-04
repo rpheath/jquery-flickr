08bb0d8711d5b5f722e0c2898fdc6d41aaf29ff7
dangreco <dan.greco@live.com>
2018-10-04 13:17:26 -0400

    Bring up to spec with modern JS syntax

--- jquery.flickr.js
+++ jquery.flickr.js
@@ -4,15 +4,14 @@
  * written for jQuery 1.3.2
  * by Ryan Heath (http://rpheath.com)
  *****************************************/
-(function($) {
+(($) => {
   // core extensions
   $.extend({
     // determines if an object is empty
     // $.isEmpty({})             // => true
     // $.isEmpty({user: 'rph'})  // => false
-    isEmpty: function(obj) {
-      for (var i in obj) { return false }
-      return true
+    isEmpty: (obj) => {
+      return Object.keys(obj).length == 0
     }
   })
   
@@ -20,12 +19,12 @@
   $.flickr = {
     // the actual request url
     // (constructs extra params as they come in)
-    url: function(method, params) {
-      return 'http://api.flickr.com/services/rest/?method=' + method + '&format=json' +
-        '&api_key=' + $.flickr.settings.api_key + ($.isEmpty(params) ? '' : '&' + $.param(params)) + '&jsoncallback=?'
+    url: (method, params) => {
+      let api_key = $.flickr.settings.api_key;
+      return `http://api.flickr.com/services/rest/?method=${method}&format=json&api_key=${api_key}${($.isEmpty(params) ? '' : '&' + $.param(params))}&jsoncallback=?`
     },
     // translate plugin image sizes to flickr sizes
-    translate: function(size) {
+    translate: (size) => {
       switch(size) {
         case 'sq': return '_s' // square
         case 't' : return '_t' // thumbnail
@@ -35,52 +34,51 @@
       }
     },
     // determines what to do with the links
-    linkTag: function(text, photo, href) {
-      if (href === undefined) href = ['http://www.flickr.com/photos', photo.owner, photo.id].join('/')      
-      return '<a href="' + href + '" title="' + photo.title + '">' + text + '</a>'
+    linkTag: (text, photo, href) => {
+      if (href === undefined) href = `http://www.flickr.com/photos/${photo.owner}/${photo.id}`
+      return `<a href="${href}" title="${photo.title}">${text}</a>`
     }
   }
   
   // helper methods for thumbnails
   $.flickr.thumbnail = {
-    src: function(photo, size) {
+    src: (photo, size) => {
       if (size === undefined) size = $.flickr.translate($.flickr.settings.thumbnail_size)
-      return 'http://farm' + photo.farm + '.static.flickr.com/' + photo.server + 
-        '/' + photo.id + '_' + photo.secret + size + '.jpg'
+      return `http://farm${photo.farm}.static.flickr.com/${photo.server}/${photo.id}_${photo.secret}${size}.jpg`
     },
     imageTag: function(image) {
-      return '<img src="' + image.src + '" alt="' + image.alt + '" />'
+      return `<img src="${image.src}" alt="${image.alt}" />`
     }
   }
   
   // accepts a series of photos and constructs
   // the thumbnails that link back to Flickr
-  $.flickr.thumbnail.process = function(photos) {
-    var thumbnails = $.map(photos.photo, function(photo) {
-      var image = new Image(), html = '', href = undefined
+  $.flickr.thumbnail.process = (photos) => {
+    let thumbnails = $.map(photos.photo, (photo) => {
+      let image = new Image(), html = '', href = undefined
 
       image.src = $.flickr.thumbnail.src(photo)
       image.alt = photo.title
 
-      var size = $.flickr.settings.link_to_size
+      let size = $.flickr.settings.link_to_size
       if (size != undefined && size.match(/sq|t|s|m|o/)) 
         href = $.flickr.thumbnail.src(photo, $.flickr.translate(size))
       
       html = $.flickr.linkTag($.flickr.thumbnail.imageTag(image), photo, href)
         
-      return ['<li>' + html + '</li>']
+      return [`<li>${html}</li>`]
     }).join("\n")
     
     return $('<ul class="flickr"></ul>').append(thumbnails)
   }
   
   // handles requesting and thumbnailing photos
-  $.flickr.photos = function(method, options) {
-    var options = $.extend($.flickr.settings, options || {}),
+  $.flickr.photos = (method, options) => {
+    let options = $.extend($.flickr.settings, options || {}),
         elements = $.flickr.self, photos
     
-    return elements.each(function() {
-      $.getJSON($.flickr.url(method, options), function(data) {
+    return elements.each(() => {
+      $.getJSON($.flickr.url(method, options), (data) => {
         photos = (data.photos === undefined ? data.photoset : data.photos)
         elements.append($.flickr.thumbnail.process(photos))
       })
@@ -91,25 +89,25 @@
   // note: options available to each method match that of Flickr's docs
   $.flickr.methods = {
     // http://www.flickr.com/services/api/flickr.photos.getRecent.html
-    photosGetRecent: function(options) {
+    photosGetRecent: (options) => {
       $.flickr.photos('flickr.photos.getRecent', options)
     },
     // http://www.flickr.com/services/api/flickr.photos.getContactsPublicPhotos.html
-    photosGetContactsPublicPhotos: function(options) {
+    photosGetContactsPublicPhotos: (options) => {
       $.flickr.photos('flickr.photos.getContactsPublicPhotos', options)
     },
     // http://www.flickr.com/services/api/flickr.photos.search.html
-    photosSearch: function(options) {
+    photosSearch: (options) => {
       $.flickr.photos('flickr.photos.search', options)
     },
     // http://www.flickr.com/services/api/flickr.photosets.getPhotos.html
-    photosetsGetPhotos: function(options) {
+    photosetsGetPhotos: (options) => {
       $.flickr.photos('flickr.photosets.getPhotos', options)
     }
   }
   
   // the plugin
-  $.fn.flickr = function(options) {
+  $.fn.flickr = (options) => {
     $.flickr.self = $(this)
     
     // base configuration
